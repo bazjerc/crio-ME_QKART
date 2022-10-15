d77b5c4539e65e7f8a283535c968405c251257f3
blaz-jerenec <blaz.jerenec@gmail.com>
2022-10-15 09:37:55 +0000

    make checkpoint

--- src/components/Products.js
+++ src/components/Products.js
@@ -14,6 +14,7 @@ import { config } from "../App";
 import Footer from "./Footer";
 import Header from "./Header";
 import ProductCard from "./ProductCard";
+import Cart from "./Cart";
 import "./Products.css";
 
 // Definition of Data Structures used
