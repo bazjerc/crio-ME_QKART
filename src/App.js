243e5b2d8229f30b86460e3b7b35ed0a3afffe51
blaz-jerenec <blaz.jerenec@gmail.com>
2022-10-22 09:30:55 +0000

    “ME_QKART_FRONTEND_V2_MODULE_DEPLOYMENT”

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import Register from "./components/Register";
-import ipConfig from "./ipConfig.json";
+/* import ipConfig from "./ipConfig.json"; */
 import { Route, Switch } from "react-router-dom";
 import Login from "./components/Login";
 import Products from "./components/Products";
@@ -7,7 +7,7 @@ import Checkout from "./components/Checkout";
 import Thanks from "./components/Thanks";
 
 export const config = {
-  endpoint: `http://${ipConfig.workspaceIp}:8082/api/v1`,
+  endpoint: `https://blaz-jerenec-crio-qkart.herokuapp.com/api/v1`,
 };
 
 function App() {
